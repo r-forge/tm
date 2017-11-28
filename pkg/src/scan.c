421ec7b2669c0b7f037e9242ae25685702c57a0d
khornik (Kurt Hornik) <Kurt.Hornik@wu.ac.at>
2017-11-28 17:04:19 +0000

    Vectorize scan tokenizer at C level.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@1521 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/scan.c
+++ pkg/src/scan.c
@@ -6,8 +6,8 @@ static int is_space_or_ascii_punct(int c) {
     return(isspace(c) || (ispunct(c) && isascii(c)));
 }
 
-SEXP _tm_scan(SEXP x, SEXP which) {
-    SEXP y, this;
+static SEXP tm_scan_one(SEXP this, int (*test) ()) {
+    SEXP y;
     Rboolean skip;
     int size = 256, i, j, nb = 0, ne = 0, u, v, w;
     int *beg, *end;
@@ -15,20 +15,6 @@ SEXP _tm_scan(SEXP x, SEXP which) {
     char c, *t, *p;
     cetype_t e;
 
-    int (*test) () = isspace;
-
-    if(LENGTH(which) > 0) {
-	PROTECT(this = AS_INTEGER(which));
-	w = INTEGER(this)[0];
-	if(w == 1)
-	    test = is_space_or_ascii_punct;
-	UNPROTECT(1);
-    }
-
-    if(LENGTH(x) < 1)
-	return NEW_CHARACTER(0);
-
-    this = STRING_ELT(x, 0);
     if(this == NA_STRING) {
 	return ScalarString(NA_STRING);
     }
@@ -83,6 +69,52 @@ SEXP _tm_scan(SEXP x, SEXP which) {
     Free(end);
 
     UNPROTECT(1);
+
+    return y;
+}
+
+
+SEXP _tm_scan(SEXP x, SEXP which) {
+
+    SEXP y, z, this;
+    R_xlen_t i, j, k, nx, ny;
+    int w;
+
+    int (*test) () = isspace;
+
+    if(LENGTH(which) > 0) {
+	PROTECT(this = AS_INTEGER(which));
+	w = INTEGER(this)[0];
+	if(w == 1)
+	    test = is_space_or_ascii_punct;
+	UNPROTECT(1);
+    }
+
+    nx = LENGTH(x);
+
+    if(nx < 1)
+	return NEW_CHARACTER(0);
+
+    if(nx == 1)
+	return tm_scan_one(STRING_ELT(x, 0), test);
+
+    PROTECT(z = NEW_LIST(nx));
+    ny = 0;
+    for(i = 0; i < nx; i++) {
+	this = tm_scan_one(STRING_ELT(x, i), test);
+	SET_VECTOR_ELT(z, i, this);
+	ny += LENGTH(this);
+    }
+    // Now unlist.
+    k = 0;
+    PROTECT(y = NEW_STRING(ny));
+    for(i = 0; i < nx; i++) {
+	this = VECTOR_ELT(z, i);
+	for(j = 0; j < LENGTH(this); j++, k++)
+	    SET_STRING_ELT(y, k, STRING_ELT(this, j));
+    }
+
+    UNPROTECT(2);
 
     return y;
 }
